875c53f96286efeade993ed92a084a8c4912c5a2
fkrryy <alfikrideo1525@gmail.com>
2026-06-21 04:44:45 +0700

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -11,5 +11,6 @@ DB_CONFIG = {
     "user": os.environ.get("DB_USER", "postgres"),
     "password": os.environ.get("DB_PASSWORD", "123"),
     "host": os.environ.get("DB_HOST", "localhost"),
-    "port": int(os.environ.get("DB_PORT", 5432))
+    "port": int(os.environ.get("DB_PORT", 5432)),
+    "connect_timeout": 5
 }
